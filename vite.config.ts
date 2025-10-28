27c3d3f7d5fcb8466180eeeff823d882e7d5c15f
behkha <behzadkhanlar@gmail.com>
2025-10-28 20:46:57 +0330

    removed action

--- vite.config.ts
+++ vite.config.ts
@@ -3,47 +3,25 @@ import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 
-export default defineConfig(({ command, mode }) => {
-  if (mode === 'demo') {
-    return {
-      plugins: [vue(), tailwindcss()],
-      resolve: {
-        alias: {
-          "@": path.resolve(__dirname, "./src"),
-        },
-      },
-      base: './',
-      build: {
-        outDir: 'dist-demo',
-        rollupOptions: {
-          input: path.resolve(__dirname, 'src/demo/index.html')
-        }
-      },
-      server: {
-        port: 3000
-      }
-    }
-  }
-
-  return {
-    plugins: [vue(), tailwindcss()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
+export default defineConfig({
+  plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: './',
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/lib/index.ts"),
+      name: "Vueson",
+      fileName: (format) => `vueson.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: { vue: "Vue" },
       },
     },
-    build: {
-      lib: {
-        entry: path.resolve(__dirname, "src/lib/index.ts"),
-        name: "Vueson",
-        fileName: (format) => `vueson.${format}.js`,
-      },
-      rollupOptions: {
-        external: ["vue"],
-        output: {
-          globals: { vue: "Vue" },
-        },
-      },
-    },
-  }
-})
+  },
+});
